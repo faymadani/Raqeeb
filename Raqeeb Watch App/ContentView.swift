1250338387b29344153d00e92963bc1e7cbaa9f4
0xwav <raghadmohammed553@gmail.com>
2024-12-11 12:54:02 +0300

    updates

---
+++ Raqeeb Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RaqeebForWatchOs Watch App
+//
+//  Created by Raghad on 11/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Raqeeb Watch App/RaqeebForWatchOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaqeebForWatchOsApp.swift
+//  RaqeebForWatchOs Watch App
+//
+//  Created by Raghad on 11/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Raqeeb_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
