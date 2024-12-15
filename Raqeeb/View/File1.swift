9310166334854eeea0f120460efebed52c6909c4
faymadani <faymadani99@gmail.com>
2024-12-15 09:42:18 +0300

    update1

--- Raqeeb/View/File1.swift
+++ Raqeeb/View/File1.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import SwiftUI
+import SwiftData
+
+@Model
+class UserData {
+    var userName: String
+    var userStatus: String
+    var contactName: String
+    var seizureDetails: [String]
+
+    init(
+        userName: String = "محمد",
+        userStatus: String = "كيف حال صحتك اليوم؟",
+        contactName: String = "أمي",
+        seizureDetails: [String] = [
+            "نوع النوبة: نوبة جزئية",
+            "مدة النوبة: ٢:٣٣",
+            "تاريخ النوبة: ٦/٦/١٤٤٦"
+        ]
+    ) {
+        self.userName = userName
+        self.userStatus = userStatus
+        self.contactName = contactName
+        self.seizureDetails = seizureDetails
+    }
+}
